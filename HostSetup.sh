80a4186e921a8f9880e6775c83b459afc09c659e
Tabisch <tobithieman@gmail.com>
2023-10-11 14:07:11 +0200

    Update HostSetup.sh

--- HostSetup.sh
+++ HostSetup.sh
@@ -14,6 +14,6 @@ if grep -q "iface enp1s0 inet dhcp" /etc/network/interfaces; then
     sudo sed -i "/iface\senp1s0\sinet\sdhcp/c\iface enp1s0 inet static\n\taddress ${IP}\n\tnetmask ${NETMASK}\n\tgateway ${GATEWAY}" /etc/network/interfaces
 fi
 
-echo -e "nameserver ${DNSSERVER}" | sudo tee /etc/resolv.conf
+echo -e "nameserver ${DNSSERVER}" | sudo tee /etc/resolv.conf &> /dev/null
 
 echo -e "$(hostname --short)\tIN\tA\t${IP}"
