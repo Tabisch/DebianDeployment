b3e79e39fe2d1edcc6b194de1a052698e45ffbbc
Tabisch <tobithieman@gmail.com>
2023-10-11 15:30:14 +0200

    Add files via upload

---
+++ installKubernetes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo swapoff -a
+sudo sed -i "/.*swap.*/d" /etc/fstab
+
+sudo modprobe br_netfilter
+sudo sysctl net.ipv4.ip_forward=1
+
+sudo apt-get update
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl containerd
