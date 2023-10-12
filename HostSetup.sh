4b9a1958d952c46930dee08ccc6fe80de85cb12b
Tabisch <tobithieman@gmail.com>
2023-10-12 13:51:38 +0200

    Update HostSetup.sh

--- HostSetup.sh
+++ HostSetup.sh
@@ -7,8 +7,8 @@ read -p "NETMASK:" NETMASK
 read -p "GATEWAY:" GATEWAY
 read -p "DNSSERVER:" DNSSERVER
 
-sudo hostnamectl hostname "${HOSTNAME}.${DOMAIN}"
 sudo sed -i "/.*$(hostname --short).*/c\127.0.1.1\t${HOSTNAME}.${DOMAIN}\t${HOSTNAME}" /etc/hosts
+sudo hostnamectl hostname "${HOSTNAME}.${DOMAIN}"
 
 if grep -q "iface enp1s0 inet dhcp" /etc/network/interfaces; then
     sudo sed -i "/iface\senp1s0\sinet\sdhcp/c\iface enp1s0 inet static\n\taddress ${IP}\n\tnetmask ${NETMASK}\n\tgateway ${GATEWAY}" /etc/network/interfaces
